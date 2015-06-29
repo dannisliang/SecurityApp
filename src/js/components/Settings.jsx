93c1d8542015dc64e705263d705759a2055b69d0
Justin Schrader <icd2k3@gmail.com>
2015-06-29 07:48:39 -0700

    progress on settings panel

--- src/js/components/Settings.jsx
+++ src/js/components/Settings.jsx
@@ -31,9 +31,6 @@ export default React.createClass({
 		SettingsStore.removeChangeListener(this._onChange);
 	},
 	// USER INPUT EVENTS //////////////////
-	_handleToggleDebug: function() {
-		SettingsActions.toggleDebug();
-	},
 	_handleChangeSensitivity: function(event) {
 		// this converts 0-100% to the actual sensitivity value
 		let sensitivity = (((100 - event.target.value) * (this.props.maxSensitivity - this.props.minSensitivity)) / 100) + this.props.minSensitivity;
@@ -58,31 +55,40 @@ export default React.createClass({
 	// RENDERING ////////////////////////
 	render: function() {
 		// this takes the actual sensitivity val and converts it to 0-100% to be more clear to user
+		// TODO: all sliders should follow this principal
 		let sensitivityPercent = Math.round(100 - (((this.state.sensitivity - this.props.minSensitivity) * 100) / (this.props.maxSensitivity - this.props.minSensitivity)));
 		let motionDetected = this.props.motionDetected ? 'Motion detected' : 'All clear';
 		return (
 			<div id="settings-container" className="absolute">
-				<a onClick={this._handleClose} href="#" title="Close">X</a>
-				<div>
-					<button onClick={this._handleToggleDebug}>Toggle Debug</button>
-				</div><div>
-					<label>Motion Sensitivity</label>
-					<input type="range" min="0" max="100" defaultValue={sensitivityPercent} onChange={this._handleChangeSensitivity} />
-					{sensitivityPercent} | {this.state.sensitivity}
-				</div><div>
-					<label>Motion Grid Size</label>
-					<input type="range" min="10" max="100" defaultValue={this.state.motionZoneDensity} onChange={this._handleChangePixelDensity} />
-					{this.state.motionZoneDensity}
-				</div><div>
-					<label>Frame Capture Speed</label>
-					<input type="range" min="1" max="60" defaultValue={this.state.fps} onChange={this._handleChangeFPS} />
-					{this.state.fps}
-				</div><div>
-					<label>Active Zones Needed</label>
-					<input type="range" min="1" max="100" defaultValue={this.state.activeZonesNeeded} onChange={this._handleChangeActiveZonesNeeded} />
-					{this.state.activeZonesNeeded}
+				<div id="settings-header">
+					<h1>Settings</h1>
+					<a onClick={this._handleClose} href="#" title="Close this settings panel" className="close-button">
+						<span className="close-button-inner"></span>
+					</a>
+					<span>
+						Mugshot Ninja is fully adjustable to suit your needs. Make sure the room is well lit for the best results. If you’re confused about what a setting is, just move your cursor over it.
+					</span>
 				</div>
-				{motionDetected}
+				<div id="settings-container">
+					<form>
+						<fieldset>
+							<label>Motion Sensitivity</label>
+							<input type="range" min="0" max="100" defaultValue={sensitivityPercent} onChange={this._handleChangeSensitivity} />
+						</fieldset>
+						<fieldset>
+							<label>Motion Grid Size</label>
+							<input type="range" min="10" max="100" defaultValue={this.state.motionZoneDensity} onChange={this._handleChangePixelDensity} />
+						</fieldset>
+						<fieldset>
+							<label>Frame Capture Speed</label>
+							<input type="range" min="1" max="60" defaultValue={this.state.fps} onChange={this._handleChangeFPS} />
+						</fieldset>
+						<fieldset>
+							<label>Active Zones Needed</label>
+							<input type="range" min="1" max="100" defaultValue={this.state.activeZonesNeeded} onChange={this._handleChangeActiveZonesNeeded} />
+						</fieldset>
+					</form>
+				</div>
 			</div>
 		);
 	},
